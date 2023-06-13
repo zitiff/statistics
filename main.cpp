34eebcccb73f3eff492262b928c3d5e9b1fab254
@mochalova <mochalova.oi@nppgamma.ru>
2023-06-13 22:24:58 +0300

    Разделены папки headers и sources

--- main.cpp
+++ main.cpp
@@ -1,37 +1,8 @@
 #include <iostream>
 #include <limits>
 
-class IStatistics {
-public:
-	virtual ~IStatistics() {}
-
-	virtual void update(double next) = 0;
-	virtual double eval() const = 0;
-	virtual const char * name() const = 0;
-};
-
-class Min : public IStatistics {
-public:
-	Min() : m_min{std::numeric_limits<double>::max()} {
-	}
-
-	void update(double next) override {
-		if (next < m_min) {
-			m_min = next;
-		}
-	}
-
-	double eval() const override {
-		return m_min;
-	}
-
-	const char * name() const override {
-		return "min";
-	}
-
-private:
-	double m_min;
-};
+#include "headers/min.h"
+#pragma once
 
 int main() {
 
